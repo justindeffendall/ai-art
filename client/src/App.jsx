55d0f74a738e872d0966533eae40f71221bc6a54
jddeff <jddeff@gmail.com>
2023-08-02 14:56:11 -0700

    Navigation bug on "create post" page addressed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,15 @@ const App = () => {
   return (
     <BrowserRouter>
     <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
-      <Link to="/">
+      <Link to="ai-art">
         <img src={logo} alt='logo' className='w-28 object-contain'/>
       </Link>
-      <Link to="/create-post" className='font-enter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md' >Create</Link>
+      <Link to="ai-art/create-post" className='font-enter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md' >Create</Link>
     </header>
     <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/create-post' element={<CreatePost />} />
+        <Route path='/ai-art' element={<Home />} />
+        <Route path='/ai-art/create-post' element={<CreatePost />} />
       </Routes>
     </main>
     </BrowserRouter>
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,7 +18,7 @@ const CreatePost = () => {
     if (form.prompt){
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://ai-art-0om2.onrender.com/api/v1/dalle", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -46,7 +46,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://ai-art-0om2.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -55,7 +55,7 @@ const CreatePost = () => {
         })
 
         await response.json();
-        navigate('/');
+        navigate('/ai-art');
       } catch (err) {
         alert(err);
       } finally {
